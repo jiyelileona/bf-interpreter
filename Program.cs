4c80fa6f9e23cba85ee89144f90fa1e31513fc46
Jiye Li <15832778446@163.com>
2021-03-27 23:44:29 -0500

    make sure able to read text from other file

--- Program.cs
+++ Program.cs
@@ -2,11 +2,13 @@
 
 namespace bfInterpreter
 {
-    class Program
+  class Program
+  {
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
+      string str = System.IO.File.ReadAllText(@".\input.bf");
+
+      System.Console.WriteLine(str);
     }
+  }
 }
